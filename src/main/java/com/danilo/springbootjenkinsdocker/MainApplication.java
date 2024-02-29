ede9b56c7be820a111905a86b30bdf0701f90a20
danilogv <danilo.gv@hotmail.com>
2024-02-29 17:19:09 -0300

    Add configuration in properties and import spring web

--- src/main/java/com/danilo/springbootjenkinsdocker/MainApplication.java
+++ src/main/java/com/danilo/springbootjenkinsdocker/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJenkinsDockerApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootJenkinsDockerApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
